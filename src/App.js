679e0605cfe3d2d5522677b804adc54b33f1f210
feriany_ahmed <ahmed.feryani.dev@gmail.com>
2021-09-10 09:33:15 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,86 @@
 import "./App.css";
-import imageInSrc from "./imageInSrc.jpg";
-import "./style.css";
-import myVideo from "./myVideo.mp4";
-function App() {
-  return (
-    <div className="App">
-      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
-        <h1 className="title red">FERYANI AHMED</h1>
-        <br />
-        <img src={imageInSrc} alt="img" />
-        <br />
-        <img src="/imageInPublic.jpg" alt="img" />
-      </div>
-      <video width="320" height="240" controls>
-        <source src={myVideo} type="video/mp4" />
-      </video>
-    </div>
-  );
-}
 
-export default App;
+import "./style.css";
+
+import React, { Component } from "react";
+
+import cx from "classnames";
+
+export default class App extends Component {
+  state = {
+    toDoList: [],
+    isFilter: "all",
+  };
+
+  handelSubmit = (e) => {
+    e.preventDefault();
+    const val = e.target.todo.value;
+    const toDo = this.state.toDoList;
+    toDo.push({ title: val, isDone: false, id: Date.now() });
+    this.setState({ toDoList: toDo });
+    e.target.todo.value = "";
+    console.log(this.state);
+  };
+
+  handelDelete = (idToDel) => {
+    const toDo = this.state.toDoList;
+    const indexItem = toDo.findIndex((item) => item.id === idToDel);
+    toDo.splice(indexItem, 1);
+    this.setState({ toDoList: toDo });
+  };
+
+  handelDone = (index) => {
+    const toDo = this.state.toDoList;
+    toDo[index].isDone = !this.state.toDoList[index].isDone;
+    this.setState({ toDoList: toDo });
+  };
+
+  handelFilter = (type) => {
+    this.setState({ isFilter: type });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <form onSubmit={this.handelSubmit}>
+            <input type="text" name="todo" />
+            <button type="submit">add list</button>
+          </form>
+        </div>
+        {this.state.toDoList
+          .filter((item) => {
+            const filter = this.state.isFilter;
+            console.log(item);
+            if (filter === "done") {
+              return item.isDone;
+            }
+            if (filter === "undone") {
+              return !item.isDone;
+            }
+            return true;
+          })
+          .map((toDo, index) => {
+            return (
+              <div key={index}>
+                <div className={cx({ done: toDo.isDone })}>
+                  {toDo.title}
+                </div>
+                <button onClick={() => this.handelDone(index)}>
+                  {toDo.isDone ? "done" : "undone"}
+                </button>
+                <button onClick={() => this.handelDelete(toDo.id)}>
+                  delete
+                </button>
+              </div>
+            );
+          })}
+        <button onClick={() => this.handelFilter("done")}>Filter Done</button>
+        <button onClick={() => this.handelFilter("undone")}>
+          Filter UnDone
+        </button>
+        <button onClick={() => this.handelFilter("all")}>all list</button>
+      </div>
+    );
+  }
+}
